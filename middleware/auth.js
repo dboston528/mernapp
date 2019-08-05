7e2e74f2a8bc8906a833314382c6d630478f8936
DeAndre Boston <dboston3@yahoo.com>
2019-08-04 23:24:30 -0500

    Update middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,4 +11,12 @@ module.exports = function(req, res, next) {
   }
 
   // Verify
+  try {
+    const decoded = jwt.verify(token, config.get('jwSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
 };
