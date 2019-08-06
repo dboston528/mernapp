e7f9e286fc1def4b9c0584c8d0ca1cd77393ea9f
DeAndre Boston <dboston3@yahoo.com>
2019-08-05 23:12:13 -0500

    Update auth routh

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const config = require('comfig');
+const config = require('config');
 
 module.exports = function(req, res, next) {
   //get token from header
@@ -7,7 +7,7 @@ module.exports = function(req, res, next) {
 
   //check if not token
   if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denid' });
+    return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
   // Verify
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route   GET api/auth
 // @desc    Test Route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
