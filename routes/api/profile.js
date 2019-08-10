beeac1d360b2c2c90b95225797e8798d2d53f0ef
DeAndre Boston <dboston3@yahoo.com>
2019-08-10 11:32:50 -0500

    update DELETE route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -148,4 +148,24 @@ router.get('/user/:user_id', async (req, res) => {
 // @route   DELETE api/profile
 // @desc    Delete profile, user & post
 // @access  Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    //@todo - remove user posts
+    //remove profile
+    await Profile.findOneAndRemove({
+      user: req.user.id
+    });
+    //remove user
+    await User.findOneAndRemove({
+      _id: req.user.id
+    });
+
+    res.json({ msg: 'user removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
