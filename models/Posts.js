4ba6977601b17736821271f941dc24fef742385c
DeAndre Boston <dboston3@yahoo.com>
2019-08-26 23:33:07 -0500

    Updare posts route

--- models/Posts.js
+++ models/Posts.js
@@ -27,7 +27,7 @@ const PostSchema = new Schema({
   comments: [
     {
       user: {
-        type: Schema.Types.ObjectiD,
+        type: Schema.Types.ObjectId,
         ref: 'users'
       },
       text: {
