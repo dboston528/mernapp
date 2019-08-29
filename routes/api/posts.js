f0cd6f27a7ab314dab75d156e6e5adeff7b0dad6
DeAndre Boston <dboston3@yahoo.com>
2019-08-28 21:23:17 -0500

    fix posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -29,12 +29,12 @@ router.post(
     try {
       const user = await User.findById(req.user.id).select('-password');
 
-      const newPost = {
+      const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id
-      };
+      });
 
       const post = await newPost.save();
       res.json(post);
