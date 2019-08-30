6ae8395a94404e8278590b86d252d5e68692854d
DeAndre Boston <dboston3@yahoo.com>
2019-08-29 23:21:54 -0500

    get all post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,7 +9,7 @@ const User = require('../../models/User');
 
 // @route   POST api/posts
 // @desc    Create a post
-// @access  Privat4e
+// @access  Private
 router.post(
   '/',
   [
@@ -44,5 +44,25 @@ router.post(
     }
   }
 );
+
+// @route   GET api/posts
+// @desc    get all post
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
