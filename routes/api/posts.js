e22d68263ae3d6ca4bb04c562e8b77754efc5d9f
DeAndre Boston <dboston3@yahoo.com>
2019-09-09 19:53:21 -0500

    Update like post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -109,6 +109,25 @@ router.delete('/:id', auth, async (req, res) => {
 // @desc    Like a post
 // @access  Private
 
-router.put('/like/:id', auth, async(req, res));
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //check if post has alrady been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
