dba690dea937b94f513e659553a05d9291a6134c
DeAndre Boston <dboston3@yahoo.com>
2019-09-12 22:31:20 -0500

    Update delete comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -201,4 +201,43 @@ router.post(
   }
 );
 
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete comment
+// @access  Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    );
+
+    //Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    //get remove index
+    const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    conslole.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
