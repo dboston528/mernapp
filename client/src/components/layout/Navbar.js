4837b3edefefbd31e8c040648d5738a319b221a3
DeAndre Boston <dboston3@yahoo.com>
2020-02-12 08:44:50 -0600

    Update logo

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> Black_Dev
+          <i className="fas fa-fist-raised"></i> Black_Dev
         </Link>
       </h1>
       <ul>
